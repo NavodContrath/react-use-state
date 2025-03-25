b767add95d9c84a63f4366138cfb2d07ec4957a7
NavodContrath <navod.contrath@gmail.com>
2025-03-25 20:02:42 +0100

    refactor Language component to accept props and manage display logic

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,30 @@ function DescriptionList() {
       {
         languages.map((language) => {
           return (
-            <Language />
+            <Language key={`language-${language.id}`}
+              title={language.title}
+              inShow={inShow === language.id}
+              showHandler={() => setInShow(inShow === language.id ? 0 : language.id)}
+            >
+              <div>
+                {language.content}
+              </div>
+            </Language>
           )
         })
       }
     </div>
   )
 }
-function Language() {
+function Language({ title, children, inShow, showHandler }) {
   return (
-    <></>
+    <>
+      <button type='button' onClick={showHandler}>{title}</button>
+      <div className="displayer">
+        <h3>{inShow && title}</h3>
+        {inShow && children}
+      </div>
+    </>
   )
 }
 
